c6b5e0f9a2f02b1a638009a84eab90d03559cf4d
Sergey Zhirkov <zhirkov_55250@mail.ru>
2021-05-03 23:19:23 +0500

    solve educational problems about Stream of primitives

---
+++ Problems/A stream without zero/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+class StreamOfPrimitives {
+
+    public static LongStream getLongStream(int n) {
+        return LongStream.iterate(-n, k -> k + 1)
+                .limit(2 * n + 1)
+                .filter(k -> k != 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        String result = getLongStream(n).mapToObj(e -> e)
+                .map(Object::toString)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+    }
+}
