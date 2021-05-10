40a70c6f60cfa6e009e596489aa6664a4a62985d
Sergey Zhirkov <zhirkov_55250@mail.ru>
2021-05-10 20:23:23 +0500

    solve educational problems about Static Nested classes and Local Inner classes

---
+++ Problems/Make slices/src/Main.java
@@ -0,0 +1,26 @@
+class Apple {
+
+    private String appleVariety;
+
+    public Apple(String appleVariety) {
+        this.appleVariety = appleVariety;
+    }
+
+    void cutApple() {
+
+        class Knife {
+            void makeSlices() {
+                System.out.println("Apple " + appleVariety + " is ready to be eaten!");
+            }
+        }
+
+
+        Knife knife = new Knife();
+        knife.makeSlices();
+    }
+
+    public static void main(String[] args) {
+        Apple apple = new Apple("Gala");
+        apple.cutApple();
+    }
+}
