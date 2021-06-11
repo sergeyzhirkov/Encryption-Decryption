8395a4ac0ab4322b5b0f667246b64aea6921334a
Sergey Zhirkov <zhirkov_55250@mail.ru>
2021-06-11 23:16:20 +0500

    solve educational problems about Executor service

---
+++ Problems/Check whether a number is prime/src/Main.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        while (scanner.hasNext()) {
+            int number = scanner.nextInt();
+            if (number != 1)
+                executor.submit(new PrintIfPrimeTask(number));
+        }
+        executor.shutdown();
+    }
+}
+
+class PrintIfPrimeTask implements Runnable {
+    private final int number;
+
+    public PrintIfPrimeTask(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        BigInteger bigInteger = BigInteger.valueOf(number);
+        boolean probablePrime = bigInteger.isProbablePrime(number);
+        if (probablePrime) {
+            System.out.println(number);
+        }
+    }
+}
